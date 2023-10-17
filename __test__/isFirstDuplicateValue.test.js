cd8aff1986766b1948f308aca57ccb5a27b953b3
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-10-17 19:18:48 -0400

    changing expect statement in test

--- __test__/isFirstDuplicateValue.test.js
+++ __test__/isFirstDuplicateValue.test.js
@@ -3,21 +3,21 @@ const isFirstDuplicateValue = require("isFirstDuplicateValue.js");
 describe("", () => {
   it("Should return the value of the first duplicate value", () => {
     const array = [9, 13, 6, 2, 3, 5, 5, 5, 3, 2, 2, 2, 2, 4, 3];
-    expect(isFirstDuplicateValue(array).toEqual(5));
+    expect(isFirstDuplicateValue(array).toBe(5));
   });
 
   it("Should return the value of the first duplicate value", () => {
     const array = [7, 6, 5, 3, 6, 4, 3, 5, 2];
-    expect(isFirstDuplicateValue(array).toEqual(6));
+    expect(isFirstDuplicateValue(array).toBe(6));
   });
 
   it("Should return the -1 if no duplicate values", () => {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    expect(isFirstDuplicateValue(array).toEqual(-1));
+    expect(isFirstDuplicateValue(array).toBe(-1));
   });
 
   it("Should return the -1 if no duplicate values", () => {
     const array = [1];
-    expect(isFirstDuplicateValue(array).toEqual(-1));
+    expect(isFirstDuplicateValue(array).toBe(-1));
   });
 });
